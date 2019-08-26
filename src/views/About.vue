ac36dc4b27924e7ab9938e44001ded061dc8b49f
Jonathan Bogdanowicz <jhonny.devguru@gmail.com>
2019-08-26 18:33:32 +0800

    Initial commit

---
+++ src/views/About.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>This is TodoList for vue framework use.</p>
+  </div>
+</template>
+
+<style scoped>
+.about {
+  padding: 16px 12px;
+  text-align: left;
+}
+</style>
